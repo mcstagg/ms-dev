8a9de4f1111203ff835cad74b276b352b29e70d0
mcstagg <mcstagg@madisoncollege.edu>
2021-05-12 14:25:07 -0500

    Added Pics to Cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import './App.css';
+import mike from './mike.png';
 import Banner from './Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col, Button, Card } from 'react-bootstrap';
 
 const App = () => {
 
@@ -7,6 +10,35 @@ const App = () => {
 
     <>
       <Banner />
+
+      <Row className="">
+        <Col className="pl-5 pt-5 ">
+          <Card className="">
+            <Card.Img variant="top" src={mike} />
+            <Card.Body>
+              <Card.Title>Projects</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="p-5">
+        <Card className="width-auto">
+            <Card.Img variant="top" src={mike} />
+            <Card.Body>
+              <Card.Title>About Me</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </>
   );
 }
