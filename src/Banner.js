a462a4041f1ead33abe3d80e52a558ffa527b13c
mcstagg <mcstagg@madisoncollege.edu>
2021-05-12 13:22:36 -0500

    Added Banner Component

---
+++ src/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+const Banner = () => {
+  
+  return (
+
+    <Row className="bg-dark p-5">
+      <Col>
+        <h1 className="text-center text-light">Mike Stagg Dev</h1>
+      </Col>
+    </Row>
+  )    
+}
+
+export default Banner;
