3982a0edf73dba3e6911789a76a93e9f21698271
mcstagg <mcstagg@madisoncollege.edu>
2021-05-16 02:48:44 -0500

    Added a third card and formatted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Banner from './components/Banner';
 import ResumeModal from './components/ResumeModal';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col, Button, Card } from 'react-bootstrap';
+import { Row, Col, Button, Card, CardDeck } from 'react-bootstrap';
 import AboutMeModal from './AboutMeModal';
 
 const App = () => {
@@ -24,17 +24,20 @@ const App = () => {
     <div className="lock-min pb-3">
       <Banner showResumeModal={showResumeModal}/>
 
-      <Row className="pl-3 pr-3 mr-0 pr-0">
-        <Col className="pr-0">
-          <Card className="tiles mt-3 float-right">
+      <Row className="">
+        <Col className="ml-3 mr-3">
+        <CardDeck className="justify-content-center">
+          <Card className="tiles mt-3">
             <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/concrete.PNG`} />
-            <Card.Body className="">
-              <Card.Title>Projects</Card.Title>
+            <Card.Body className="pt-0">
+              <Card.Title>Concrete Volume Calculator</Card.Title>
               <Card.Text>
-                Here are the projects I have created and worked on. 
+                This app is a construction materials calculator that processes
+                dimesnions and calculates and displays the total cubic yardage 
+                for the pour. The customer is then able to place the order.
               </Card.Text>
               <Button
-                className="pr-3"
+                className="mt-2"
                 variant="primary" 
                 onClick={
                   ()=> window.open(
@@ -43,8 +46,21 @@ const App = () => {
                   )
                 }
               >
-                Concrete Volume Calculator
+                View Concrete Volume Calculator
               </Button>
+            </Card.Body>
+          </Card>
+          <Card className="tiles mt-3">
+            <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/jenga.PNG`} />
+            <Card.Body className="pt-2">
+              <Card.Title>Jenga Table Top Compainion App</Card.Title>
+              <Card.Text>
+                This app is a companion for the board game Jenga. The player
+                is able to add and select player names. Then the game begins 
+                and players are able to keep track of moves, time, and other 
+                stats. When the game is over post game stats are displayed and 
+                the leaderboard is calculated. 
+              </Card.Text>
               <Button
                 className="mt-1" 
                 variant="primary" 
@@ -55,13 +71,11 @@ const App = () => {
                   )
                 }
               >
-                Jenga Table Top Companion
+                View Jenga Table Top Companion
               </Button>
             </Card.Body>
           </Card>
-        </Col>
-        <Col className="pr-0">
-        <Card className="tiles mt-3 pb-5 float-left">
+          <Card className="tiles mt-3 pb-5">
             <Card.Img variant="top" src={`${process.env.PUBLIC_URL}/mike.png`} />
             <Card.Body className="">
               <Card.Title>About Me</Card.Title>
@@ -73,10 +87,11 @@ const App = () => {
                 variant="primary"
                 onClick={showAboutMeModal}
               >
-                Learn More
+                Learn More About Mike
               </Button>
             </Card.Body>
           </Card>
+        </CardDeck>
         </Col>
       </Row>
 
